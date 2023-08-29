c7b3d7876cbc79d02a18ac9a6d324e70ec12040c
VioletaCD <31123695@uaz.edu.mx>
2023-08-29 11:15:04 -0600

    Actividad 03

--- p07-estadisticas/Program.cs
+++ p07-estadisticas/Program.cs
@@ -2,19 +2,22 @@
 int n=0;
 double[] valores = null;
 double mayor=0, menor=0, promedio=0, varianza=0, desvest=0;
+
 Console.WriteLine("Programa que genera estadisticas de numeros\n");
-Console.Write("Cuantos elementos deseas calcular ? ");
+Console.Write("Cuantos elementos deseas calcular: ");
+
 n=int.Parse(Console.ReadLine());
 if(n>MAX) {
-Console.WriteLine("El maximo número de elementos es {0}",MAX);
+    Console.WriteLine("El maximo número de elementos es {0}",MAX);
+
 }
 else {
-valores = new double[n];
-// Leer valores del usuario
-for(int i=0; i<n; i++) {
-Console.Write($"valores[{i+1}] = ");
-valores[i]=double.Parse(Console.ReadLine());
-}
+    valores = new double[n];
+    // Leer valores del usuario
+    for(int i=0; i<n; i++) {
+        Console.Write($"valores[{i+1}] = ");
+        valores[i]=double.Parse(Console.ReadLine());
+    }
 }
 // Motrar los datos
 Console.WriteLine($"Las calificaciones introducidas son : {est.Mostrar(valores)}");
@@ -32,40 +35,36 @@ Console.WriteLine("La varianza es : {0:f2}",varianza);
 Console.WriteLine("La desviacion estandar: {0:f2}",desvest);
 public static class est {
 public static string Mostrar(double[] v) {
-return String.Join(",",v);
+    return String.Join(",",v);
 }
 public static double Mayor(double[] v) {
-double m=v[0];
-for(int i=0; i<v.Length; i++) {
-if(v[i]>m) m=v[i];
+    double m=v[0];
+    for(int i=0; i<v.Length; i++) {
+        if(v[i]>m) m=v[i];
 
 }
-return m;
+    return m;
 }
 public static double Menor(double[] v) {
-double m=v[0];
-for(int i=0; i<v.Length; i++) {
-if(v[i]<m) m=v[i];
-
+    double m=v[0];
+    for(int i=0; i<v.Length; i++) {
+        if(v[i]<m) m=v[i];
 }
 return m;
 }
 public static double Promedio(double[] v) {
-double m=0;
-for(int i=0; i<v.Length; i++) {
-
-m+=v[i];
-
+    double m=0;
+    for(int i=0; i<v.Length; i++) {
+        m+=v[i];
 }
-return m / v.Length;
+    return m / v.Length;
 }
 public static double Varianza(double[] v, double p) {
-double m=0;
-for(int i=0; i<v.Length; i++) {
-m+=Math.Pow(v[i]-p,2);
-
+    double m=0;
+    for(int i=0; i<v.Length; i++) {
+        m+=Math.Pow(v[i]-p,2);
 }
-return m / v.Length;
+    return m / v.Length;
 }
-public static double Desviacion(double var ) => Math.Sqrt(var);
+    public static double Desviacion(double var ) => Math.Sqrt(var);
 }
